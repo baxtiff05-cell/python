273c182391702a7d91df197dfdc296fd5fae32fd
Baxtiyor <baxtiff05@gmail.com>
2025-10-25 16:40:05 +0500

    25/10/2025

---
+++ python_list.py
@@ -0,0 +1,55 @@
+"""
+25.10.2025
+ro'yhatlar bilan ishlash.Metodlar va function lar bilan ishlash methods(.sort())
+function(sum(),min(),max(), sorted()),list(),)
+Ruxsatlardan nusha olish
+Tuple() o'zgarmas ro'yhat 
+"""
+# davlatlar = ['Ozbekiston', 'AQSH', 'Rossiya', 'Xitoy', 'Turkiya']
+# moshinalar = list['BMW', 'GM', 'Toyota', 'Hyundai', 'Kia']
+# sonlar = list((2,5,7,8))
+
+
+# print(sonlar)
+# print(davlatlar)
+# print(moshinalar)
+
+
+# moshinalar.sort()
+# print(moshinalar)
+
+
+# sonlar.sort(reverse=True)
+# print(sonlar)
+
+
+# sonlar1 = list(range(1, 30 , 2))
+# print(sonlar1)
+
+
+# sonlar2 = list(range(-80,523,3))
+# print(sonlar2)
+# print(len(sonlar2))
+
+# sonlar3 = [89,-98,63-2,-96,98,32,120,157,369,895,1235,2558,-75]
+# print(max(sonlar3))
+# print(min(sonlar3))
+
+# print(sum(max + min))
+
+# davlatlar = ['Ozbekiston', 'AQSH', 'Rossiya', 'Xitoy', 'Turkiya']
+# country = davlatlar
+
+# print("davlatlar nomli ro'yhat:",davlatlar)
+# print("country nomli ro'yhat:",country)
+
+# country.insert('Turkiya')
+# print(country)
+
+# print(davlatlar)
+# print(country)
+
+# toqson = list(range(1,100,2))
+# print("asl ko'rinishi :",toqson)
+# print("1 dan 7 gacha :",toqson[0:7])
+# print(toqson[:])
