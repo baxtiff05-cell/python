b2a79a9dd495b017f3d9572452d2c9dc29bfb045
Baxtiyor <baxtiff05@gmail.com>
2025-10-26 15:58:30 +0500

    26/10/2025

--- amaliyot.py
+++ amaliyot.py
@@ -1,45 +1,44 @@
-#ismlar ro'yhati va ularga qisqa habar yuborish 
-ismlar = ["Shoxrux", "Muhammadali", "Kamolbek"]
-print("Salom", ismlar[0] + ", bugun maktab bormi?")
-print(ismlar[1] + ", maktabga boramizmi?")
+# hayvon = "it"
+# ovqati = "go'sht"
 
-#sonlar ro'yhati 
-sonlar = [5, -3, 10, 2.5]
-print("Dastlabki sonlar:", sonlar)
-#sonlarni o'zgartirish
-sonlar[0] = sonlar[0] + 10
-sonlar[1] = sonlar[1] * -1
-print("O'zgargan sonlar:", sonlar)
+# print(hayvon.capitalize())
+# print(ovqati.lower())
+# print(ovqati.upper())
+# print(hayvon.title())
 
-#tarixiy,zamonaviy shaxslar 
-t_shaxslar = ["Imom Buxoriy", "Amir Temur"]
-z_shaxslar = ["Bill Gates", "Elon Musk"]
-#ro'yhatdan bitta shaxsni chiqarib olish
-tarixiy = t_shaxslar.pop(0)
-zamonaviy = z_shaxslar.pop(0)
-print(f"Men tarixiy shaxslardan {tarixiy} bilan,")
-print(f"zamonaviy shaxslardan esa {zamonaviy} bilan suhbat qilishni istar edim.")
+# meva = "  olma  "
+# print("" + meva.lstrip())
+# print("" + meva.rstrip())
+# print("" + meva.strip())      
 
-#mehmonlar ro'yxati
-friends = []
-friends.append("Jasur")
-friends.append("Nodir")
-friends.append("Bekzod")
-friends.append("Olim")
-friends.append("Sardor")
-#birini chaqira olmaymiz
-print("Mehmonlar:", friends)
-friends.remove("Bekzod")
-print("Yangilangan mehmonlar ro'yxati:", friends)
-#boshi va o'rtasiga yangi mehmonlar qo'shish
-friends.insert(0, "Jmashid")
-friends.insert(2, "Azam")
-friends.append("Doston")
-print("Yangi mehmonlar ro'yxati:", friends)
+# meva = "olma"
+# print("" + meva.center(20))
+# print("" + meva.ljust(20))            
+# print("" + meva.rjust(20))    
 
-#yangi mehmonlar ro'yxati
-mehmonlar = []
-mehmonlar.append(friends.pop(0))
-mehmonlar.append(friends.pop(1))
-print("Kelgan mehmonlar:", mehmonlar)
-print("Kelmaganlar:", friends)
+# meva = "olma"
+# print(meva.zfill(20))   
+# print(meva.zfill(4))    
+# print(meva.zfill(2))
+# print(meva.zfill(1))    
+
+# day = "5"
+# print(day.zfill(2))
+# print(day.zfill(3))
+# print(day.zfill(1))
+# print(day.zfill(0))
+# print(day.zfill(4))
+
+# davlat = "o'zbekiston"
+# print(davlat.upper())
+# print(davlat.lower())
+# print(davlat.title())
+# print(davlat.capitalize())
+# print(davlat.center(30))
+# print(davlat.ljust(30))
+# print(davlat.rjust(30))
+# print(davlat.zfill(20))
+
+
+
+
