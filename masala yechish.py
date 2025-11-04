002be693e14978396c2e03b630ee8f04bb3829b4
Baxtiyor <baxtiff05@gmail.com>
2025-11-04 16:24:06 +0500

    04/11/2025

---
+++ masala yechish.py
@@ -0,0 +1,24 @@
+# 1
+# k = input("k ni kiriting: ")
+# n = int(input("n (n>0) ni kiriting: "))
+# for i in range(n):
+#     print(k)
+
+# 2
+# a = int(input("a: "))
+# b = int(input("b (a<b): "))
+# for x in range(a, b+1):
+#     print(x)
+
+# 3
+# a = int(input("a: "))
+# b = int(input("b (a<b): "))
+# for x in range(b-1, a, -1):
+#     print(x)
+
+# 4
+# p = float(input("1 kg konft narxi (so'm): "))
+# for kg in range(1, 11):
+#     print(f"{kg} kg: {kg * p}")
+
+
