4edf98b3c7b4c1493c4c4e085aa99601b6cfcaaf
Baxtiyor <baxtiff05@gmail.com>
2025-11-05 13:02:45 +0500

    .

---
+++ yangi 5.py
@@ -0,0 +1,21 @@
+viloyatlar = [xorazm, andijon, buxoro, jizzax, qashqadaryo, qorovulbozor, namangan, navoiy, samarqand, surxondaryo, sirdaryo, toshkent_viloyati, fargona, ]
+print(viloyatlar)
+for viloyat in viloyatlar:
+    print(viloyat)
+    if viloyat == "toshkent viloyati":
+        break
+print("Dastur tugadi.")
+xorazm = "Xorazm"
+andijon = "Andijon"
+buxoro = "Buxoro"
+jizzax = "Jizzax"
+qashqadaryo = "Qashqadaryo"
+qorovulbozor = "Qorovulbozor"
+namangan = "Namangan"
+
+navoiy = "Navoiy"
+samarqand = "Samarqand"
+surxondaryo = "Surxondaryo"
+sirdaryo = "Sirdaryo"
+toshkent_viloyati = "Toshkent viloyati"
+fargona = "Farg'ona"
