907a9bdceb7c7de20934763be9629177708599ba
Baxtiyor <baxtiff05@gmail.com>
2025-11-05 12:49:17 +0500

    05/11/2025

---
+++ yangi 3.py
@@ -0,0 +1,7 @@
+ism = [oybek, anvar, aziz]
+for ism in ism:
+    print(f"Salom, {ism.title()}!")
+# yangi 3.
+# Dasturda ism o'zgaruvchisi ro'yxat nomi sifatida ishlatilgan.
+# Bunday nomlash chalkashliklarga olib kelishi mumkin.
+# Ro'yxat nomini o'zgartirish tavsiya etiladi, masalan, ismlar deb nomlash mumkin.
---
+++ yangi 4.py
@@ -0,0 +1,8 @@
+hayvonlar = ['mushuk', 'it', 'quyon']
+for hayvon in hayvonlar:
+    print(f"{hayvon.title()} juda ajoyib hayvon.")
+# yangi 4.
+# Dasturda hayvon o'zgaruvchisi ro'yxat nomi
+# sifatida ishlatilgan. Bunday nomlash chalkashliklarga olib kelishi mumkin.
+# Ro'yxat nomini o'zgartirish tavsiya etiladi,
+# masalan, hayvonlar_ro'yxati deb nomlash mumkin.
