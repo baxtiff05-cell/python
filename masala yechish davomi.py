99819b3960ac8c741dad2a28a127346cdae1d15b
Baxtiyor <baxtiff05@gmail.com>
2025-11-06 16:22:34 +0500

    06/11/2025

---
+++ masala yechish davomi.py
@@ -0,0 +1,62 @@
+# # 5
+# narx = float(input("1 kg konfetning narxini kiriting: "))
+# for kg in range(1, 11):
+#     kg_kg = kg / 10
+#     summa = kg_kg * narx
+#     print(f"{kg_kg} kg konfetning narxi: {summa} so'm")
+
+# 6
+# son = int(input("Istalgan butun son kiriting: "))
+# for i in range(1, 11):
+#  kopaytma = son * i
+# print(f"{son} x {i} = {kopaytma}")
+
+
+# 7
+# n = int(input("n sonini kiriting (n>0): "))
+# for son in range(1, n + 1):
+#  kub = son     
+#  print(f"{son} ning kubi: {kub}")
+
+# 8
+# n = int(input("n sonini kiriting (n>0): "))
+# yigindi = 0
+# for son in range(1, n + 1):
+#     yigindi += son
+# print(f"1 dan {n} gacha bo'lgan sonlarning yig'indisi: {yigindi}")
+
+#     9
+# n = int(input("n sonini kiriting (n>0): "))
+# yigindi = 0
+# for son in range(2, n + 1, 2):
+#      yigindi += son
+# print(f"1 dan {n} gacha bo'lgan juft sonlarning yig'indisi: {yigindi}")
+
+
+#10
+
+# n = int(input("n sonini kiriting (n>0): "))
+# yigindi = 0
+# for son in range(1, n + 1, 2):
+#     yigindi += son
+# print(f"1 dan {n} gacha bo'lgan toq sonlarning yig'indisi: {yigindi}")
+
+
+
+#   11
+
+# n = int(input("n sonini kiriting (n>0): "))
+# kopaytma = 1
+# for son in range(1, n + 1):
+#     kopaytma *= son
+# print(f"1 dan {n} gacha bo'lgan sonlarning ko'paytmasi: {kopaytma}")
+
+
+
+# 12
+
+# n = int(input("n sonini kiriting (n>0): "))
+# faktorial = 1
+# for son in range(1, n + 1):
+#     faktorial *= son
+# print(f"{n}! ning qiymati: {faktorial}")
